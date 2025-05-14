a1cd041f75315515d92bd1932732a53d3cab86ec
buj17 <igor.ssobolev.06@gmail.com>
2025-05-14 19:34:06 +0300

    music_manager.py update

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from db.managers import MusicManager, UserManager
 
 if __name__ == '__main__':
     user_manager = UserManager()
-    print(MusicManager(create_session()).search_music('a'))
+    musics = MusicManager(create_session()).search_music('dhooma')
+    print(MusicManager(create_session()).get_music_image_url(musics[0].id))
